32d6408adf4f2c81e230e3aea40479985547a610
Yugal-Bhagat <yugalbhagat23@gmail.com>
2025-03-08 18:04:19 +0530

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Replace 'MySide' with your repository name
 export default defineConfig({
   plugins: [react()],
+  base: '/MySide/', // 👈 This is required for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
